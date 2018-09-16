fd14713bc4d4d41743069e387fe1140624e7e206
Anand Singh <singh.ak700@gmail.com>
2018-09-16 14:46:55 +0530

    Prevent null message transfer

--- socket_server/server.php
+++ socket_server/server.php
@@ -1,165 +1,120 @@
-<?php
-$host = '0.0.0.0'; //host
-$port = 12345; //port
-$null = NULL; //null var
+<html>
+    <head>
+    </head>
 
-$allowed_origins = ["*"]; //Set allowed origins here, by default all origins are allowed to connect
+    <body>
+        <video id="localVideo" autoplay muted style="width:40%;"></video>
+        <video id="remoteVideo" autoplay style="width:40%;"></video>
 
-//Create TCP/IP sream socket
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-//reuseable port
-socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+        <br />
 
-//bind socket to specified host
-socket_bind($socket, 0, $port);
-
-//listen to port
-socket_listen($socket);
-
-//create & add listning socket to the list
-$clients = array($socket);
-
-//start endless loop, so that our script doesn't stop
-while (true) {
-	//manage multiple connections
-	$changed = $clients;
-	//returns the socket resources in $changed array
-	socket_select($changed, $null, $null, 0, 10);
-	
-	//check for new socket
-	if (in_array($socket, $changed)) {
-		$socket_new = socket_accept($socket); //accpet new socket
-		$clients[] = $socket_new; //add socket to client array
-		
-		$header = socket_read($socket_new, 1024); //read data sent by the socket
-		perform_handshaking($header, $socket_new, $host, $port); //perform websocket handshake
-		
-		socket_getpeername($socket_new, $ip); //get ip address of connected socket
-		$response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' connected'))); //prepare json data
-		send_message($response); //notify all users about new connection
-		
-		//make room for new socket
-		$found_socket = array_search($socket, $changed);
-		unset($changed[$found_socket]);
-	}
-	
-	//loop through all connected sockets
-	foreach ($changed as $changed_socket) {	
-		
-		//check for any incomming data
-		while(socket_recv($changed_socket, $buf, 1024, 0) >= 1)
-		{
-			$received_text = unmask($buf); //unmask data
-			$tst_msg = json_decode($received_text, true); //json decode 
-	
-			//prepare data to be sent to client
-			$response_text = mask($tst_msg);
-			send_message($response_text); //send data
-			break 2; //exist this loop
-		}
-		
-		$buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
-		if ($buf === false) { // check disconnected client
-			// remove client for $clients array
-			$found_socket = array_search($changed_socket, $clients);
-			@socket_getpeername($changed_socket, $ip);
-			unset($clients[$found_socket]);
-			
-			//notify all users about disconnected connection
-			$response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' disconnected')));
-			send_message($response);
-		}
-	}
-}
-// close the listening socket
-socket_close($socket);
-
-function send_message($msg)
-{
-	global $clients;
-	foreach($clients as $changed_socket)
-	{
-		@socket_write($changed_socket,$msg,strlen($msg));
-	}
-	return true;
-}
+        <input type="button" id="start" onclick="start(true)" value="Start Video"></input>
 
 
-//Unmask incoming framed message
-function unmask($text) {
-	$length = ord($text[1]) & 127;
-	if($length == 126) {
-		$masks = substr($text, 4, 4);
-		$data = substr($text, 8);
-	}
-	elseif($length == 127) {
-		$masks = substr($text, 10, 4);
-		$data = substr($text, 14);
-	}
-	else {
-		$masks = substr($text, 2, 4);
-		$data = substr($text, 6);
-	}
-	$text = "";
-	for ($i = 0; $i < strlen($data); ++$i) {
-		$text .= $data[$i] ^ $masks[$i%4];
-	}
-	return $text;
-}
 
-//Encode message for transfer to client.
-function mask($text)
-{
-	$b1 = 0x80 | (0x1 & 0x0f);
-	$length = strlen($text);
-	
-	if($length <= 125)
-		$header = pack('CC', $b1, $length);
-	elseif($length > 125 && $length < 65536)
-		$header = pack('CCn', $b1, 126, $length);
-	elseif($length >= 65536)
-		$header = pack('CCNN', $b1, 127, $length);
-	return $header.$text;
-}
+        <script type="text/javascript">
+            
+            var localVideo;
+            var remoteVideo;
+            var peerConnection;
+            var peerConnectionConfig = {'iceServers': [{'url': 'stun:stun.services.mozilla.com'}, {'url': 'stun:stun.l.google.com:19302'}]};
 
-//handshake new client.
-function perform_handshaking($receved_header,$client_conn, $host, $port)
-{
-	global $allowed_origins;
+            navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+            window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+            window.RTCIceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate;
+            window.RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
 
-	$headers = array();
-	$lines = preg_split("/\r\n/", $receved_header);
-	foreach($lines as $line)
-	{
-		$line = chop($line);
-		if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
-		{
-			$headers[$matches[1]] = $matches[2];
-		}
-	}
+            function pageReady() {
+                localVideo = document.getElementById('localVideo');
+                remoteVideo = document.getElementById('remoteVideo');
 
-	$allow_handshake = false;
+                console.log("connecting socket");
 
-	//Allow handshake from allowed origin only
-	foreach ($allowed_origins as $key => $value) {
-		if(fnmatch($value, $headers['Origin'])){
-			$allow_handshake = true;
-		}
-	}
+                serverConnection = new WebSocket('wss://localhost/wss2/');
+                serverConnection.onmessage = gotMessageFromServer;
+                serverConnection.onopen = function(){console.log("socket connected");}
+                serverConnection.onerror = function(){console.error("socket error");}
 
+                var constraints = {
+                    video: true,
+                    audio: true,
+                };
 
-	if(!$allow_handshake){
-		echo "Socket connection denied due to disallowed origin.\n";
-		return ;		
-	}
+                if(navigator.getUserMedia) {
+                    navigator.getUserMedia(constraints, getUserMediaSuccess, getUserMediaError);
+                } else {
+                    alert('Your browser does not support getUserMedia API');
+                }
+            }
 
-	$secKey = $headers['Sec-WebSocket-Key'];
-	$secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-	//hand shaking header
-	$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-	"Upgrade: websocket\r\n" .
-	"Connection: Upgrade\r\n" .
-	"WebSocket-Origin: $host\r\n" .
-	"Sec-WebSocket-Accept:$secAccept\r\n\r\n";
-	socket_write($client_conn,$upgrade,strlen($upgrade));
-}
+            function getUserMediaSuccess(stream) {
+                localStream = stream;
+                localVideo.src = window.URL.createObjectURL(stream);
+            }
+
+            function getUserMediaError(error) {
+                console.log(error);
+            }
+
+            function start(isCaller) {
+                peerConnection = new RTCPeerConnection(peerConnectionConfig);
+                peerConnection.onicecandidate = gotIceCandidate;
+                peerConnection.onaddstream = gotRemoteStream;
+                peerConnection.addStream(localStream);
+
+                if(isCaller) {
+                    peerConnection.createOffer(gotDescription, createOfferError);
+                }
+            }
+
+            function gotDescription(description) {
+                console.log('got description');
+                peerConnection.setLocalDescription(description, function () {
+                    serverConnection.send(JSON.stringify({'sdp': description}));
+                }, function() {console.log('set description error')});
+            }
+
+            function gotIceCandidate(event) {
+                if(event.candidate != null) {
+                    serverConnection.send(JSON.stringify({'ice': event.candidate}));
+                }
+            }
+
+            function gotRemoteStream(event) {
+                console.log('got remote stream');
+                remoteVideo.src = window.URL.createObjectURL(event.stream);
+            }
+
+            function createOfferError(error) {
+                console.log(error);
+            }
+
+            function gotMessageFromServer(message) {                   
+
+                if(!message.data){ return ;}
+                
+                var signal = JSON.parse(message.data);
+
+                if(signal.type=="system"){ return ;}
+
+                if(!peerConnection) start(false);
+
+                if(signal.sdp) {
+                    peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function() {
+                        if(signal.sdp.type == 'offer') {
+                            peerConnection.createAnswer(gotDescription, createAnswerError);
+                        }
+                    });
+                } else if(signal.ice) {
+                    peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice));
+                }
+            }
+
+        </script>
+
+        <script type="text/javascript">
+            pageReady();
+        </script>
+
+    </body>
+</html>
