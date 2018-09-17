978de1b00a3d6a328eb25abd559955b1ecf8394a
hack4mer <singh.ak700@gmail.com>
2018-09-17 12:29:23 +0530

    Add ratchet

--- socket_server/server.php
+++ socket_server/server.php
@@ -1,174 +1,44 @@
 <?php
-$host = '0.0.0.0'; //host
-$port = 12345; //port
-$null = NULL; //null var
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
 
-$allowed_origins = ["*"]; //Set allowed origins here, by default all origins are allowed to connect
+// Make sure composer dependencies have been installed
+require __DIR__ . '/../vendor/autoload.php';
 
-//Create TCP/IP sream socket
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-//reuseable port
-socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+/**
+ * server.php
+ * Send any incoming messages to all connected clients (except sender)
+ */
+class GWatchSocketServer implements MessageComponentInterface {
+    protected $clients;
 
-//bind socket to specified host
-socket_bind($socket, 0, $port);
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
 
-//listen to port
-socket_listen($socket);
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
 
-//create & add listning socket to the list
-$clients = array($socket);
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from != $client) {
+                $client->send($msg);
+            }
+        }
+    }
 
-//start endless loop, so that our script doesn't stop
-while (true) {
-	//manage multiple connections
-	$changed = $clients;
-	//returns the socket resources in $changed array
-	socket_select($changed, $null, $null, 0, 10);
-	
-	//check for new socket
-	if (in_array($socket, $changed)) {
-		$socket_new = socket_accept($socket); //accpet new socket
-		$clients[] = $socket_new; //add socket to client array
-		
-		$header = socket_read($socket_new, 1024); //read data sent by the socket
-		perform_handshaking($header, $socket_new, $host, $port); //perform websocket handshake
-		
-		socket_getpeername($socket_new, $ip); //get ip address of connected socket
-		$response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' connected'))); //prepare json data
-		send_message($response); //notify all users about new connection
-		
-		//make room for new socket
-		$found_socket = array_search($socket, $changed);
-		unset($changed[$found_socket]);
-	}
-	
-	//loop through all connected sockets
-	foreach ($changed as $changed_socket) {	
-		
-		//check for any incomming data
-		while(socket_recv($changed_socket, $buf, 1024, 0) >= 1)
-		{
-			$received_text = unmask($buf); //unmask data
-			$tst_msg = json_decode($received_text, true); //json decode 
-	
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
 
-
-
-			if(!empty($tst_msg)){
-
-				$tst_msg = json_encode($tst_msg);
-
-
-				//prepare data to be sent to client
-				$response_text = mask($tst_msg);
-				send_message($response_text); //send data
-				break 2; //exist this loop
-			}
-		}
-		
-		$buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
-		if ($buf === false) { // check disconnected client
-			// remove client for $clients array
-			$found_socket = array_search($changed_socket, $clients);
-			@socket_getpeername($changed_socket, $ip);
-			unset($clients[$found_socket]);
-			
-			//notify all users about disconnected connection
-			$response = mask(json_encode(array('type'=>'system', 'message'=>$ip.' disconnected')));
-			send_message($response);
-		}
-	}
-}
-// close the listening socket
-socket_close($socket);
-
-function send_message($msg)
-{
-	global $clients;
-	foreach($clients as $changed_socket)
-	{
-		@socket_write($changed_socket,$msg,strlen($msg));
-	}
-	return true;
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
 }
 
-
-//Unmask incoming framed message
-function unmask($text) {
-	$length = ord($text[1]) & 127;
-	if($length == 126) {
-		$masks = substr($text, 4, 4);
-		$data = substr($text, 8);
-	}
-	elseif($length == 127) {
-		$masks = substr($text, 10, 4);
-		$data = substr($text, 14);
-	}
-	else {
-		$masks = substr($text, 2, 4);
-		$data = substr($text, 6);
-	}
-	$text = "";
-	for ($i = 0; $i < strlen($data); ++$i) {
-		$text .= $data[$i] ^ $masks[$i%4];
-	}
-	return $text;
-}
-
-//Encode message for transfer to client.
-function mask($text)
-{
-	$b1 = 0x80 | (0x1 & 0x0f);
-	$length = strlen($text);
-	
-	if($length <= 125)
-		$header = pack('CC', $b1, $length);
-	elseif($length > 125 && $length < 65536)
-		$header = pack('CCn', $b1, 126, $length);
-	elseif($length >= 65536)
-		$header = pack('CCNN', $b1, 127, $length);
-	return $header.$text;
-}
-
-//handshake new client.
-function perform_handshaking($receved_header,$client_conn, $host, $port)
-{
-	global $allowed_origins;
-
-	$headers = array();
-	$lines = preg_split("/\r\n/", $receved_header);
-	foreach($lines as $line)
-	{
-		$line = chop($line);
-		if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
-		{
-			$headers[$matches[1]] = $matches[2];
-		}
-	}
-
-	$allow_handshake = false;
-
-	//Allow handshake from allowed origin only
-	foreach ($allowed_origins as $key => $value) {
-		if(fnmatch($value, $headers['Origin'])){
-			$allow_handshake = true;
-		}
-	}
-
-
-	if(!$allow_handshake){
-		echo "Socket connection denied due to disallowed origin.\n";
-		return ;		
-	}
-
-	$secKey = $headers['Sec-WebSocket-Key'];
-	$secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-	//hand shaking header
-	$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-	"Upgrade: websocket\r\n" .
-	"Connection: Upgrade\r\n" .
-	"WebSocket-Origin: $host\r\n" .
-	"Sec-WebSocket-Accept:$secAccept\r\n\r\n";
-	socket_write($client_conn,$upgrade,strlen($upgrade));
-}
+// Run the server application through the WebSocket protocol on port 8080
+$app = new Ratchet\App('localhost', 12345);
+$app->route('/', new GWatchSocketServer);
+$app->route('/echo', new Ratchet\Server\EchoServer, array('*'));
+$app->run();
