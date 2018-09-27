0568e12f40e69b22d07fdc3d9abef0b1fcbccaff
hack4mer <singh.ak700@gmail.com>
2018-09-27 18:28:50 +0530

    Remove comments

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,17 +6,6 @@ const ExtractStyles = new ExtractTextPlugin({filename: 'styles.css'});
 module.exports = {
   devServer: {
     https: true,
-   
-
-   /**
-    * Use following setttings if you wish to use self-signed certificates for the localhost dev-server
-    * For a guide on how to generate self signed-ssl certs for localhost
-    * See: https://medium.freecodecamp.org/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec
-    */
-
-    // key: fs.readFileSync(__dirname+'/path/to/server.key'),
-    // cert: fs.readFileSync(__dirname+'/path/to/server.crt'),
-    // ca: fs.readFileSync(__dirname+'/path/to/ca.pem'),
   },
   entry: path.resolve(__dirname,'./src/js/GWatch.js'),
   mode: 'development',
